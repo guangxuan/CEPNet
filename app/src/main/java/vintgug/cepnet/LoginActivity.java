e6dbc8b4508e78203b031570a86f82a8b09a6109
Swagman9000 <lee.vint.ve@gmail.com>
2015-05-22 16:58:53 +0800

    added nav drawer layout

--- app/src/main/java/vintgug/cepnet/LoginActivity.java
+++ app/src/main/java/vintgug/cepnet/LoginActivity.java
@@ -106,7 +106,7 @@ public class LoginActivity extends Activity {
                                                                 public void onClick(DialogInterface dialog, int which) {
                                                                     Intent intent = new Intent(
                                                                             LoginActivity.this,
-                                                                            HomeActivity.class);
+                                                                            TabActivity.class);
                                                                     startActivity(intent);
                                                                     finish();
                                                                 }
