d33e20e3fb768edc216496636fcb4381b2b0f81e
guangxuan <guangxuan99rulang@yahoo.com.sg>
2015-05-22 23:13:09 +0800

    Basic Profile Page

--- app/src/main/java/vintgug/cepnet/HomeActivity.java
+++ app/src/main/java/vintgug/cepnet/HomeActivity.java
@@ -142,6 +142,10 @@ public class HomeActivity extends ActionBarActivity implements ActionBar.TabList
         public Fragment getItem(int position) {
             // getItem is called to instantiate the fragment for the given page.
             // Return a PlaceholderFragment (defined as a static inner class below).
+            switch(position){
+                case 2:
+                    return ProfileFragment.newInstance();
+            }
             return PlaceholderFragment.newInstance(position + 1);
         }
 
