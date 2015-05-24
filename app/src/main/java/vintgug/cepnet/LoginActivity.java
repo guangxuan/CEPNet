da4bbc72db4e8a97ad0181391832c63583bb2ec0
guangxuan <guangxuan99rulang@yahoo.com.sg>
2015-05-24 15:44:43 +0800

    Login goes to homepage now regardless

--- app/src/main/java/vintgug/cepnet/LoginActivity.java
+++ app/src/main/java/vintgug/cepnet/LoginActivity.java
@@ -106,7 +106,7 @@ public class LoginActivity extends Activity {
                                                                 public void onClick(DialogInterface dialog, int which) {
                                                                     Intent intent = new Intent(
                                                                             LoginActivity.this,
-                                                                            TabActivity.class);
+                                                                            HomeActivity.class);
                                                                     startActivity(intent);
                                                                     finish();
                                                                 }
